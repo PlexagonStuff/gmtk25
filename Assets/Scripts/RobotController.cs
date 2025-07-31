908b1023af58235938c6c6d6dd245efb0b234a93
Avery Rair <drslug7@gmail.com>
2025-07-30 21:16:28 -0400

    Robot Controller Scripts

---
+++ Assets/Scripts/RobotController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class RobotController : MonoBehaviour {
+
+    private RobotAttachment attachment;
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
