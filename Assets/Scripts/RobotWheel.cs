4f785e5e69d2b2f1f3054fd7b7e125ee7bf63d5e
Avery Rair <drslug7@gmail.com>
2025-07-31 23:16:27 -0400

    Wheel Attachment stuff

--- Assets/Scripts/RobotWheel.cs
+++ Assets/Scripts/RobotWheel.cs
@@ -19,7 +19,6 @@ public class RobotWheel : RobotAttachment
 
     public override void UpArrow()
     {
-        Debug.Log("Up Pressed");
         //This could be changed to world once we figure out the whole collision thing
         LayerMask mask = LayerMask.GetMask("GroundLayer");
         RaycastHit2D hit = Physics2D.Raycast(transform.position + new Vector3(0, -0.5f*transform.localScale.x), -transform.up, 0.1f, mask);
